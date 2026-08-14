e0e488067a2f88c2befe7bcaebf4fa11858be9a4
Anshuman Singh <anshumansingh3697@gmail.com>
2026-08-14 14:10:55 +0530

    Time: 31 ms (8.21%), Space: 13.3 MB (85.65%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -4,15 +4,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        length=len(nums)
+        #length=len(nums)
         #nums will already sorted
         numsc=[]
-        count=0
+        #count=0
         for num in nums:
             if num not in numsc:
                 numsc.append(num)
-            else:
-                count+=1
+            #lse:
+            #    count+=1
         for i in range(len(numsc)):
             nums[i] = numsc[i]
         
