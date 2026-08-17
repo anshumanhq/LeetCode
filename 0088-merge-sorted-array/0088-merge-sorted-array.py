ec0d9206e142b5b26bafb3ddc4a4b4bd7e494ceb
Anshuman Singh <anshumansingh3697@gmail.com>
2026-08-17 21:53:26 +0530

    Time: 0 ms (100%), Space: 12.3 MB (92.2%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def merge(self, nums1, m, nums2, n):
+        """
+        :type nums1: List[int]
+        :type m: int
+        :type nums2: List[int]
+        :type n: int
+        :rtype: None Do not return anything, modify nums1 in-place instead.
+        """
+        if m==0 and n==0:
+            nums1[:]=[]
+        srtd=[]
+        for i in range(m):
+            srtd.append(nums1[i])
+        for j in range(n):
+            srtd.append(nums2[j])
+        nums1[:]=sorted(srtd)
+        return sorted(srtd)
