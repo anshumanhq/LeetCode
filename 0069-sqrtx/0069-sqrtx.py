eea5381011aad3f711292199c490cd061f449ab2
Anshuman Singh <anshumansingh3697@gmail.com>
2026-08-17 09:13:26 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.33%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -7,8 +7,17 @@ class Solution(object):
         #hme sqrt nikalna hai
         #1 ka1,2 ka 4, 3 ka 9, 4 ka 16
         #agar x=12 diya hai then left 3 aur right 4 ke bich me hai so 
-        pre,curr=0,1
-        while curr*curr<=x:
-            curr+=1
-            pre+=1
-        return pre 
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        while left <= right:
+            mid = (left + right) // 2
+            sq = mid * mid
+            if sq == x:
+                return mid
+            elif sq < x:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return right
