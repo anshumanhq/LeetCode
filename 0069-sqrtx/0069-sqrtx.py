62a89ac3a8e6988a314263248b7369c877973a09
Anshuman Singh <anshumansingh3697@gmail.com>
2026-08-17 09:09:08 +0530

    Time: 1454 ms (5.02%), Space: 12.3 MB (90.28%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        #hme sqrt nikalna hai
+        #1 ka1,2 ka 4, 3 ka 9, 4 ka 16
+        #agar x=12 diya hai then left 3 aur right 4 ke bich me hai so 
+        pre=0
+        curr=1
+        while curr*curr<=x:
+            curr+=1
+            pre+=1
+        return pre 
