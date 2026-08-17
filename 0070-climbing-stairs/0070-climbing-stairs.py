86d2a2688dbc7846f504354e2a0d2606f1e03c67
Anshuman Singh <anshumansingh3697@gmail.com>
2026-08-17 09:28:51 +0530

    Time: 0 ms (100%), Space: 12.4 MB (57.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
+        #jab hm analisys krte hain then hme pata chalta hai ye ek fabonachi series hi return kr raha hai 
+        #like for 2,3,4,5,6,7,8 ke liye 2,3,5,8,13,21,etc
         a,b=1,1
         for _ in range(n):
             a,b=b,a+b
